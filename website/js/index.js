671cc68f62af17dcf91d86956f3f295867065a25
PPTL <enyang.chua2@gmail.com>
2020-09-19 19:01:21 +0800

    增加首頁

---
+++ website/js/index.js
@@ -0,0 +1,8 @@
+function marqueeSlider(){
+    let marqueeContent = document.getElementsByClassName("marquee-content-item");
+    console.log(marqueeContent.length);
+    for(let i = 0; i < marqueeContent.length; i++){
+        marqueeContent[i].style.left = (i + 40);
+    }
+}
+marqueeSlider();
