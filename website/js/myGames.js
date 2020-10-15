07bf5992c8eec969a108a03e8b12240028933de7
brianfung420_home <brianfung420@gmail.com>
2020-10-15 21:47:44 +0800

    use axios send request

--- website/js/myGames.js
+++ website/js/myGames.js
@@ -1,3 +1,29 @@
 function launchGame(gameName){
-    alert("Links for you : http://thisisjustatestlink");
+    //alert("Links for you : http://thisisjustatestlink");
+    let Uid=sessionStorage.getItem('userID');
+	axios({
+		method:'POST',
+		baseURL:'https://4757e09f7a65.ngrok.io',
+		url:'/playGame',
+		'Content-Type':'application/json',
+		data:{
+			id : Uid,
+			gamename : gameName
+		},
+		// `timeout` 指定请求超时的毫秒数(0 表示无超时时间)
+		timeout: 1000,
+	})
+	.then(function(result){
+		console.log(result.status);
+		console.log(result.statusText);
+		console.log(result.data);
+		console.log(result.headers);
+		console.log(result.config);
+	})
+	.catch(function(error){
+		console.log(error);
+	})
 }
+
+addLoadEvent(setLIFF);
+addLoadEvent(checkLogin);
