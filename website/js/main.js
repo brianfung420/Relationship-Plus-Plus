e887ca20bba3269934ace7c2bf9d780bd6aa3b4d
brianfung420_home <brianfung420@gmail.com>
2020-10-15 21:45:09 +0800

    update function
    上傳資料到後端

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,4 @@
-function handleUploadPic(previewBox,input_file){
+function handleUploadPic(previewBox,tagName,input_file){
     let previewDiv = document.getElementById(previewBox);
     while (previewDiv.firstChild) {
         previewDiv.removeChild(previewDiv.lastChild);
@@ -16,6 +16,7 @@ function handleUploadPic(previewBox,input_file){
     
         let img = document.createElement("img");
         img.file = null;
+        img.name = tagName;
         img.classList.add("inner-img");
         img.classList.add("obj");
         img.file = file;
@@ -23,7 +24,9 @@ function handleUploadPic(previewBox,input_file){
         
         let reader = new FileReader();
         reader.onload = (function(aImg) { 
-        	return function(e) { aImg.src = e.target.result; }; })(img);
+        	return function(e) { 
+                aImg.src = e.target.result;
+            }; })(img);
         reader.readAsDataURL(file);
     }
 }
@@ -43,4 +46,73 @@ function addLoadEvent(func){
 function closeWin(winName){
   let win = document.getElementById(winName);
   win.style.display = "none";
+}
+
+function uploadImage(url,file){
+    id = sessionStorage.getItem('userID');
+    gameName = sessionStorage.getItem('gameName');
+    file.set('userId',id);
+    file.set('gameName',gameName);
+        //console.log(file);
+        // for (var key in text){
+        //     formData.append(key, text[key]);
+        // }
+        // for (var key in file){
+        //     const img_Blob = DataURITiBlob(file[key]);
+        //     const type = img_Blob.type;
+        //     let img_type=dataURI.split('/')[1];
+        //     formData.append(key, img_Blob,key+'.'+img_type);
+        // }
+    fetch(url, {    //不用set header就可以上傳資料(content-type) 
+            method: 'POST',
+            body: file,
+    })
+    .then(function(response){
+        if(response.ok){
+            return response.url;
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(function(url){
+        window.location.href = url;
+    })
+    .catch(function(err) {
+        console.log('There has been a problem with your fetch operation:', err);
+    });
+}
+
+function DataURIToBlob(dataURI) {
+    const splitDataURI = dataURI.split(',');
+    //console.log("splitDataURI:"+splitDataURI);
+    const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1]);
+    //console.log("byteString:"+byteString);
+    const mimeString = splitDataURI[0].split(':')[1].split(';')[0];
+    //console.log("mimeString:"+mimeString);
+
+    const ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++)
+        ia[i] = byteString.charCodeAt(i);
+
+    return new Blob([ia], { type: mimeString });
+}
+
+function checkLogin(){
+    let myLiffId = "";
+    myLiffId = sessionStorage.getItem('liffID');
+    console.log(myLiffId);
+    if(!myLiffId){
+        console.log("is die!");
+    }else{
+        liff.init({
+            liffId: myLiffId
+        }).then(function(){
+            //console.log("liffID:"+myLiffId);
+            if(!liff.isLoggedIn()){
+                liff.login();
+            }
+        }).catch((err) => {
+            console.log("Can't loading LIFF");
+            console.log(err);
+        });
+    }
 }
