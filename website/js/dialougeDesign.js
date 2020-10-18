2c48f8cbfb8d5d155f8747faf35f322a6a293cb9
brianfung420_home <brianfung420@gmail.com>
2020-10-19 00:27:01 +0800

    insert name attr

--- website/js/dialougeDesign.js
+++ website/js/dialougeDesign.js
@@ -7,6 +7,7 @@ function addDialougeInput(){
     newInputLable.classList.add("dialouge-title");
     newInputLable.classList.add("subtitle");
     newInputLable.innerHTML +='標題' + (dialougeAmount+1);
+    newInput.name = "dialouge-content";
     newInput.required = true;
 
     newInput.classList.add("dialouge-content");
