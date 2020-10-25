0be105b02f8f5b983bd2ae943b558a9cfbcca223
brianfung420_home <brianfung420@gmail.com>
2020-10-25 11:51:29 +0800

    update database

--- website/express.js
+++ website/express.js
@@ -6,22 +6,9 @@ const path = require('path');
 const npm = require("npm");
 const bodyParser = require('body-parser');
 const random = require('string-random')
-
 var formHandle = require('./js/formHandle.js');
 
-//const formidable = require('express-formidable')
-//app.use(formidable()); 
-
 const multer = require('multer');
-// var storage = multer.diskStorage({
-// 	destination: function (req, file, cb) {
-// 		cb(null, './userData/img/');
-// 	},
-// 	filename: function (req, file, cb) {
-// 		cb(null,  file.originalname.split('.')[0] +Date.now()+"."+file.mimetype.split('/')[1] );
-// 	}
-// });
-// var img_multer = multer({storage:storage});
 
 //LIFF
 const passport = require('passport');
@@ -53,6 +40,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -109,33 +97,6 @@ app.get('/customGame',function(req,res){
 	});
 });
 
-// function setRandomMulter(req,res,next){
-// 	let randomID = random(16);
-// 	console.log(randomID);
-// 	fs.mkdir('./game/dist/'+randomID,function(err){
-// 		if(err){
-// 			console.log(err);
-// 		}else{
-// 			console.log('create done!');
-// 		}
-// 	});
-// 	let randomPath = path.join(__dirname+'/game/dist/'+randomID);
-// 	var storage_random = multer.diskStorage({
-// 		destination: function (req, file, cb) {
-// 			console.log("dest randomId:"+randomPath);
-// 			cb(null, './game/dist/'+randomPath+"/");
-// 		},
-// 		filename: function (req, file, cb) {
-// 			cb(null,  file.originalname.split('.')[0] +"."+file.mimetype.split('/')[1] );
-// 		}
-// 	});
-	
-// 	console.log("setRandomMulter path:"+randomID);
-// 	random_multer = new multer({storage:storage_random}); 
-// 	req.body['randomId'] = randomID;
-// 	console.log(req.body['randomId']);
-// 	next();
-// }
 var sec_random_storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		fs.mkdir('./game/dist/tmp',function(err){
@@ -160,10 +121,10 @@ app.post('/customGameUpload',random_multer.any(),function(req,res,next){
 	let formData = req.body;
 
 	let LineId = formData['LineId'],GameName = formData['GameName'];
-	console.log(LineId,GameName);
+	//console.log(LineId,GameName);
 
 	//console.log('form data',formData);
-	console.log('file data',req.files);
+	//console.log('file data',req.files);
 
 	let json_text = formHandle.getJson(formData);
 	json_text['LineId'] = LineId;
@@ -230,22 +191,80 @@ app.post('/customGameUpload',random_multer.any(),function(req,res,next){
 	});
 
 	//使用非同步存進Database
-	function saveToDB(LineId,GameName,callback){
+	function saveToDB(LineId,GameName,randomPath,callback){
 		return new Promise(function (resolve,reject){
+			console.log(LineId,GameName,randomPath);
 			mongodb.connect(function (err){
 				if(err) reject(new Error(err)) ;
 				// let query = mongodb.db.collection('GameList').find({'id':dbId}).toArray();
 				// console.log(query.length);
-				mongodb.db.collection('GameList').findOneAndReplace({"LineId":{$eq:LineId},"GameName":{$eq:GameName}},json_text,{upsert:true},function(err,res){
-					if(err) reject(new Error(err));
-					console.log("1 document Replace");
-					resolve("Replace Success");
+				mongodb.db.collection('GameList').findOne({"LineId":{$eq:LineId}},function(err,result){
+					if(err){
+						console.log(err);
+					}
+					//	result有可能是空的 所以需要判斷是不是null
+					if(result==null){			//是null的話就新增資料到DB
+						let data = {};
+						data['LineId'] = LineId;
+						data['game'] = [];
+						data['game'][0] = {};
+						data['game'][0][GameName] = randomPath;
+						mongodb.db.collection('GameList').insertOne(data,function(err,res){
+							if(err) reject(new Error("Can't insert data to Database"));
+						});
+						console.log("insert Data!");
+						resolve("Insert Success");
+					}
+					else{
+						console.log("update Data");
+						let flag = 0,exist_path=0,index;
+
+						//查找DB的資料有沒有和上傳的的GameName相同
+						for(const [key,value] of Object.entries(result['game'])){
+							console.log(key,value);
+							for(const [inner_key,inner_value] of Object.entries(value)){
+								console.log(inner_key,inner_value);
+								if(inner_key===GameName){
+									flag=1;
+									exist_path = inner_value;
+									index = key;
+								}
+							}
+						}
+						console.log(index,exist_path);
+						if(flag){		//有的話就將DB的部分改成上傳的GameName和path
+							let data = {};
+							data[GameName] = randomPath;
+							let tmp = "game."+index;		//重點部分
+							mongodb.db.collection('GameList').updateOne({"LineId":{$eq:LineId}},{$set:{[tmp]:data}},function(err,res){
+								if(err){
+									console.log(err);
+									reject(new Error("Can't update data to Database"));
+								}
+								//console.log(res);
+								resolve("Update Success");
+							});
+						}else{			//還沒實測，可能需要花時間去研究
+							let data = {};
+							data[GameName] = randomPath;
+							mongodb.db.collection('GameList').updateOne({"LineId":{$eq:LineId}},{$addToSet:data},function(err,res){
+								if(err){
+									console.log(err);
+									reject(new Error("Can't update data to Database"));
+								}
+								//console.log(res);
+								resolve("Update Success");
+							});
+						}
+						
+					}
+					
 				});
 			});
 		});
 	}
 	
-	saveToDB(LineId,GameName).then(function(resp){
+	saveToDB(LineId,GameName,randomPath).then(function(resp){
 		console.log(resp);
 		next();
 	})
@@ -259,92 +278,131 @@ app.post('/customGameUpload',random_multer.any(),function(req,res,next){
 
 app.post('/customGameUpload',function(req,res){
 	console.log("I am Next!");
+	res.writeHead(200,{"Content":"text/html"});
 	res.url = './index';
-	res.redirect(302,'/index');
+	res.write(randomPath);
+	res.end();
+	console.log("res end");
 });
 
-app.post('/playGame',function(req,res){
-	// 網頁方面的請求
-	console.log("POST from playGame");
-	let data = req.body;
-	let id = data.LineId,gameName=data.gamename;
-	function checkFilePath(){
-		return new Promise(function(resolve,reject){
-			mongodb.connect(function(err){
-				if(err) reject(new Error(err));
-																				 //這部分是指除了這些以外的需要(未實作)
-				mongodb.db.collection('GameList').findOne({"LineId":id,"GameName":gameName}/*,{_id:0,'LineId':0}*/,function(err,result){
-					if(err) reject(new Error(err));
-					if(result==null){
-						reject("Not Data in databse");
-					}
-					resolve(result);
-				});
-			})
-		})
-	}
+// app.post('/playGame',function(req,res){
+// 	// 網頁方面的請求
+// 	console.log("POST from playGame");
+// 	let data = req.body;
+// 	let id = data.LineId,gameName=data.gamename;
+// 	function checkFilePath(){
+// 		return new Promise(function(resolve,reject){
+// 			mongodb.connect(function(err){
+// 				if(err) reject(new Error(err));
+// 																				 //這部分是指除了這些以外的需要(未實作)
+// 				mongodb.db.collection('GameList').findOne({"LineId":id,"GameName":gameName}/*,{_id:0,'LineId':0}*/,function(err,result){
+// 					if(err) reject(new Error(err));
+// 					if(result==null){
+// 						reject("Not Data in databse");
+// 					}
+// 					resolve(result);
+// 				});
+// 			})
+// 		})
+// 	}
 	
-	checkFilePath().then(function (dbObject){
-		console.log('Success');
-		pathName = path.join(__dirname,"./userData/json/"+dbObject.LineId+dbObject.GameName+'.json');
-		let data = JSON.stringify(dbObject);
-		//開啓檔案 讀取('r') 
-		fs.open(pathName,'r',function(err,fd){
-			if(err){
-				console.log(err);
-				fs.writeFile(pathName,data,function(err){
-					if(err){
-						console.log(err);
-						return new Error("can not write data to JSON");
-					}
-					else{
-						console.log("Success create User JSON file!");
-						return 'Create new json file';
-					}
-				});
-			}
+// 	checkFilePath().then(function (dbObject){
+// 		console.log('Success');
+// 		pathName = path.join(__dirname,"./userData/json/"+dbObject.LineId+dbObject.GameName+'.json');
+// 		let data = JSON.stringify(dbObject);
+// 		//開啓檔案 讀取('r') 
+// 		fs.open(pathName,'r',function(err,fd){
+// 			if(err){
+// 				console.log(err);
+// 				fs.writeFile(pathName,data,function(err){
+// 					if(err){
+// 						console.log(err);
+// 						return new Error("can not write data to JSON");
+// 					}
+// 					else{
+// 						console.log("Success create User JSON file!");
+// 						return 'Create new json file';
+// 					}
+// 				});
+// 			}
 
-			// var buffr = new Buffer(1024);
-			// let output;
+// 			// var buffr = new Buffer(1024);
+// 			// let output;
 
-			// fs.read(fd,function(err,bytes,buffer){
-			// 	if(err) return new Error('Can not read json');
-			// 	if(bytes>0){
-			// 		console.log(bytes+" 字元被讀取");
-			// 		output = buffr.slice(0, bytes).toString()
-   //          		console.log(output);
-			// 	}
-			// });
-			console.log("File exist(Function Inner)");	//這個會在最後才run到，在不適合的時候出現了(太晚了)		
-			return "File exist";
-		});
-	})
-	.then(function (result){
-		console.log("file is find!");
-		console.log(result);	//不知道爲什麽是undefined
-		res.writeHead(200,{'Content-Type':'application/json'});
-		res.write('Success!');
-		res.end();
-	})
-	.catch(function(error){
-		console.log(error);
-		res.writeHead(404,{'Content-Type':'application/json'});
-		res.write('Can not find user json file!');
-		res.end();
-	})
+// 			// fs.read(fd,function(err,bytes,buffer){
+// 			// 	if(err) return new Error('Can not read json');
+// 			// 	if(bytes>0){
+// 			// 		console.log(bytes+" 字元被讀取");
+// 			// 		output = buffr.slice(0, bytes).toString()
+//    //          		console.log(output);
+// 			// 	}
+// 			// });
+// 			console.log("File exist(Function Inner)");	//這個會在最後才run到，在不適合的時候出現了(太晚了)		
+// 			return "File exist";
+// 		});
+// 	})
+// 	.then(function (result){
+// 		console.log("file is find!");
+// 		console.log(result);	//不知道爲什麽是undefined
+// 		res.writeHead(200,{'Content-Type':'application/json'});
+// 		res.write('Success!');
+// 		res.end();
+// 	})
+// 	.catch(function(error){
+// 		console.log(error);
+// 		res.writeHead(404,{'Content-Type':'application/json'});
+// 		res.write('Can not find user json file!');
+// 		res.end();
+// 	})
 
 	
-	// console.log(data);
-	// res.writeHead(200,{'Content-Type':'application/json'});
-	// res.write(tmp);
-	// res.end();
-});
+// 	// console.log(data);
+// 	// res.writeHead(200,{'Content-Type':'application/json'});
+// 	// res.write(tmp);
+// 	// res.end();
+// });
 
 app.get("/playGame",function(req,res){
 	console.log("get playCustGame");
-	console.log(__dirname);
-	npm.load(()=>npm.run("game"));
-	res.end();
+	let LineId = req.body.LineId,gamename = req.body.gamename;
+	mongodb.db.collection('GameList').findOne({"LineId":{$eq:LineId}},function(err,result){
+		if(err){
+			console.log(err);
+		}
+		//	result有可能是空的 所以需要判斷是不是null
+		if(result==null){			//是null的話就新增資料到DB
+			res.writeHead(200,{'Content-Type':'application/json'});
+			res.write("No Data in database");
+			res.end();
+		}
+		else{
+			console.log("update Data");
+			//查找DB的資料有沒有和上傳的的GameName相同
+			let db_path,flag=0;
+			for(const [key,value] of Object.entries(result['game'])){
+				console.log(key,value);
+				for(const [inner_key,inner_value] of Object.entries(value)){
+					console.log(inner_key,inner_value);
+					if(inner_key===GameName){
+						flag=1;
+						db_path = inner_value;
+						index = key;
+					}
+				}
+			}
+			if(flag){
+				res.writeHead(200,{'Content-Type':'application/json'});
+				res.write("database data:"+gamename+":"+db_path);
+				res.end();
+			}else{
+				res.writeHead(200,{'Content-Type':'application/json'});
+				res.write("No this game");
+				res.end();
+			}
+			
+			//npm.load(()=>npm.run("webpackBuild"));
+		}
+	});
 });
 
 app.listen(port,function(){
--- website/js/myGames.js
+++ website/js/myGames.js
@@ -3,7 +3,7 @@ function launchGame(gameName){
     let Uid=sessionStorage.getItem('userID');
 	axios({
 		method:'POST',
-		baseURL:'https://74a924282db3.ngrok.io',
+		baseURL:'https://d2b36a1092ea.ngrok.io',
 		url:'/playGame',
 		'Content-Type':'application/json',
 		data:{
